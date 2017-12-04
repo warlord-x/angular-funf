92b4d1fc2b77806e902e187159635c748c155faf
warlord-x <gaurang.developer@gmail.com>
2017-12-04 16:20:50 +0100

    Server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule }  from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
