5fb7c4be1dba4ec331a9336d2b7412225322cd7e
warlord-x <gaurang.developer@gmail.com>
2017-12-05 10:56:03 +0100

    more components (success, warning)

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: '.alert-warning',
+  templateUrl: './warning-alert.component.html'
+})
+export class WarningAlertComponent{
+
+}
